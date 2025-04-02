a3b454e3fa2827fb0a2039eda9bed31347ce8c51
phamvietduc0411 <phamvietduc0411@gmail.com>
2025-04-02 22:18:18 +0700

    Login Page

---
+++ KahootTeamRealTimeAdmin/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using KahootTeamRealTimeAdmin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KahootTeamRealTimeAdmin.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        //Login 
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.UserNameOrEmail == "admin" && model.Password == "password")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không đúng.");
+                }
+            }
+            return View(model);
+        }
+    }
+}
+
