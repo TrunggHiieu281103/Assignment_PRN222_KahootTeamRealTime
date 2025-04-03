7291c7058b8b67baa6a0294c3f1efe5c33c75893
phamvietduc0411 <phamvietduc0411@gmail.com>
2025-04-03 14:14:14 +0700

    Change connection string

--- Repositories/Models/RealtimeQuizDbContext.cs
+++ Repositories/Models/RealtimeQuizDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Repositories.Models;
 
@@ -35,9 +36,17 @@ public partial class RealtimeQuizDbContext : DbContext
 
     public virtual DbSet<UserRoom> UserRooms { get; set; }
 
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-39B7IASC\\SQLEXPRESS;Database=RealtimeQuizDB;Uid=sa;Pwd=1;TrustServerCertificate=True;");
+    { optionsBuilder.UseSqlServer(GetConnectionString()); }
+
+    private string GetConnectionString()
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true).Build();
+        return configuration["ConnectionStrings:DefaultConnection"];
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
